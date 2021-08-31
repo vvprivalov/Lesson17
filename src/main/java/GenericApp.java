053c67a073eae13313a6ec11f91afcd91dcec523
vv.privalov <vv.privalov@yandex.ru>
2021-08-31 21:13:02 +0300

    Второе задание готово

--- src/main/java/GenericApp.java
+++ src/main/java/GenericApp.java
@@ -1,20 +1,24 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class GenericApp {
 
     public static void main(String[] args) {
 
         // Проверка первого метода
-        String[] array1 = {"1", "2", "3", "4", "5", "6"};
+        String[] array1 = {"Один", "Два", "Три", "Четыре", "Пять", "Шесть"};
         System.out.println(Arrays.toString(array1));
         changeElementArray(array1, 2, 4);
         System.out.println(Arrays.toString(array1));
 
-
+        // Проверка второго метода
+        String[] arrayStr = {"Один", "Два", "Три", "Четыре", "Пять"};
+        List arraylist = arrayToArrayList(arrayStr);
+        System.out.println(arraylist);
     }
 
 
-    public static <T> void changeElementArray(T [] array, int a, int b) {
+    public static <T> void changeElementArray(T[] array, int a, int b) {
 
         T interim;
 
@@ -22,5 +26,9 @@ public class GenericApp {
         array[a] = array[b];
         array[b] = interim;
 
+    }
+
+    public static <T> List<T> arrayToArrayList(T[] array) {
+        return Arrays.asList(array);
     }
 }
