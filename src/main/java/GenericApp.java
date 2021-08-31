8a96c6d9e46f1f6f1aab78c58a60b489b10ed95e
vv.privalov <vv.privalov@yandex.ru>
2021-08-31 19:56:14 +0300

    Первое задание готово

--- src/main/java/GenericApp.java
+++ src/main/java/GenericApp.java
@@ -1,7 +1,26 @@
+import java.util.Arrays;
+
 public class GenericApp {
 
+    public static void main(String[] args) {
 
-    public static <T> void changeElementArray(T array[]) {
+        // Проверка первого метода
+        String[] array1 = {"1", "2", "3", "4", "5", "6"};
+        System.out.println(Arrays.toString(array1));
+        changeElementArray(array1, 2, 4);
+        System.out.println(Arrays.toString(array1));
+
+
+    }
+
+
+    public static <T> void changeElementArray(T [] array, int a, int b) {
+
+        T interim;
+
+        interim = array[a];
+        array[a] = array[b];
+        array[b] = interim;
 
     }
 }
